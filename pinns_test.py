0593d5f93a50ee569356a5298143d71f533aa24a
Antoine Bensalah <antoine.bensalah@airbus.com>
2023-12-14 00:51:19 +0100

    Clean up & better parameters!

--- pinns_test.py
+++ pinns_test.py
@@ -1,6 +1,8 @@
 from PIL import Image
 
 import os
+from collections import namedtuple
+from abc import ABC, abstractmethod
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,49 +11,66 @@ import matplotlib.pyplot as plt
 import tqdm
 
 
-def save_gif_PIL(outfile, files, fps=5, loop=0):
+def save_gif_pil(outfile, files, fps=5, loop=0):
     """Helper function for saving GIFs"""
     imgs = [Image.open(file) for file in files]
     imgs[0].save(fp=outfile, format='GIF', append_images=imgs[1:],
                  save_all=True, duration=int(1000 / fps), loop=loop)
 
 
-class Oscillator:
+class CauchyProblem(ABC):
+    @abstractmethod
+    def reference_solution(self, x):
+        pass
+
+    @abstractmethod
+    def residual(self, ddy, dy, y):
+        pass
+
+    @abstractmethod
+    def cauchy_conditions(self, dy, y):
+        pass
+
+
+class Oscillator(CauchyProblem):
     """
     Equation
         m y''(x) + mu y'(x) + k y(x) = 0,  x in (0, 1)
     Initial condition
         y(0) = 1
         y'(0) = 1
+    Here
+        - dumping = mu / 2m
+        - pulsation = sqrt(k/m)
     """
-    def __init__(self, d, w0):
-        assert d < w0
-        self.d = d
-        self.w0 = w0
+    def __init__(self, dumping, pulsation):
+        assert dumping < pulsation
+        self.d = dumping
+        self.w0 = pulsation
+        self.y0 = 1.0
+        self.dy0 = 0.0
 
-    def exact_solution(self, x):
+    def reference_solution(self, x):
         """Defines the analytical solution to the 1D underdamped harmonic
         oscillator problem.
-        Equations taken from: https://beltoforion.de/en/harmonic_oscillator/"""
+        Equations taken from: https://beltoforion.de/en/harmonic_oscillator/
+        """
         w = np.sqrt(self.w0 ** 2 - self.d ** 2)
         phi = np.arctan(-self.d / w)
-        A = 1 / (2 * np.cos(phi))
+        a = 1 / (2 * np.cos(phi))
         cos = torch.cos(phi + w * x)
-        sin = torch.sin(phi + w * x)
+        # sin = torch.sin(phi + w * x)
         exp = torch.exp(-self.d * x)
-        return exp * 2 * A * cos
+        return exp * 2 * a * cos
 
-    def residual(self, dy_dxx, dy_dx, y):
+    def residual(self, ddy, dy, y):
         mu = 2 * self.d
         k = self.w0 ** 2
-        return dy_dxx + mu * dy_dx + k * y
+        return ddy + mu * dy + k * y
 
-    def fd_residual(self, x, y):
-        x = x.reshape(-1)[1:-1]
-        y = y.reshape(-1)[1:-1]
-        dy_dx = np.gradient(y, x)
-        dy_dxx = np.gradient(dy_dx, x)
-        return self.residual(dy_dxx, dy_dx, y)
+    def cauchy_conditions(self, dy, y):
+        return namedtuple("CauchyCond", ["y0", "dy0"])(y[0] - self.y0,
+                                                       dy[0] - self.dy0)
 
 
 class SinActivation(nn.Module):
@@ -95,8 +114,8 @@ def plot_result(x_exact, y_exact, xp, yh, epoch, file_name):
     plt.scatter(xp, -0 * torch.ones_like(xp), s=60, color="tab:green",
                 alpha=0.4,
                 label='Physics loss training locations')
-    l = plt.legend(loc=(1.01, 0.34), frameon=False, fontsize="large")
-    plt.setp(l.get_texts(), color="k")
+    legend = plt.legend(loc=(1.01, 0.34), frameon=False, fontsize="large")
+    plt.setp(legend.get_texts(), color="k")
     plt.xlim(-0.05, 1.05)
     plt.ylim(-1.1, 1.1)
     plt.text(1.065, 0.7, "Training step: %i" % (epoch + 1), fontsize="xx-large",
@@ -106,22 +125,27 @@ def plot_result(x_exact, y_exact, xp, yh, epoch, file_name):
                 pad_inches=0.1, dpi=100, facecolor="white")
 
 
-def plot_learning(epoch_list,
-                  loss_residual_list,
-                  loss_y0_list,
-                  loss_dy_dx_0_list,
-                  total_loss_list):
-    fig = plt.figure()
+def plot_losses(losses):
+    fig = plt.figure(figsize=(10, 10))
     ax = fig.subplots(2, 2)
-    ax[0][0].semilogy(epoch_list, total_loss_list, label="Total loss")
-    ax[0][1].semilogy(epoch_list, loss_dy_dx_0_list, label="loss dy/dx[0]**2")
-    ax[1][0].semilogy(epoch_list, loss_y0_list, label="loss (y[0] - 1)**2")
-    ax[1][1].semilogy(epoch_list, loss_residual_list, label="loss residual")
+    epoch_list = range(len(losses.residual))
+    ax[0][0].semilogy(epoch_list, losses.total,
+                      color="black",
+                      label="Total loss")
+    ax[0][1].semilogy(epoch_list, losses.residual,
+                      color="red",
+                      label="loss residual")
+    ax[1][0].semilogy(epoch_list, losses.init_value,
+                      color="green",
+                      label="loss (y[0] - 1)**2")
+    ax[1][1].semilogy(epoch_list, losses.init_derivative,
+                      color="green",
+                      label="loss dy/dx[0]**2")
     [(a.grid(), a.legend()) for aa in ax for a in aa]
     fig.savefig("loss_convergence")
 
 
-def train_from_data(oscillator: Oscillator):
+def train_from_data(cauchy_problem: CauchyProblem):
     plots_dir = "./plots"
     if not os.path.exists(plots_dir):
         os.makedirs(plots_dir)
@@ -130,7 +154,7 @@ def train_from_data(oscillator: Oscillator):
     # get the analytical solution over the full domain
     nx = 500
     x = torch.linspace(0, 1, nx).view(-1, 1)
-    y = oscillator.exact_solution(x).view(-1, 1)
+    y = cauchy_problem.reference_solution(x).view(-1, 1)
 
     # slice out a small number of points from the LHS of the domain
     n_data = 50
@@ -171,20 +195,34 @@ def train_from_data(oscillator: Oscillator):
 
             files.append(file_name)
 
-    save_gif_PIL("nn.gif", files, fps=20, loop=0)
+    save_gif_pil("nn.gif", files, fps=20, loop=0)
 
 
-def train_pinns(oscillator: Oscillator):
+class SavedLosses:
+    def __init__(self):
+        self.total = []
+        self.residual = []
+        self.init_value = []
+        self.init_derivative = []
+
+    def save(self, total, residual, init_value, init_derivative):
+        self.total.append(total)
+        self.residual.append(residual)
+        self.init_value.append(init_value)
+        self.init_derivative.append(init_derivative)
+
+
+def train_pinns(cauchy_problem: CauchyProblem):
     plots_dir = "./plots"
     if not os.path.exists(plots_dir):
         os.makedirs(plots_dir)
     # get the analytical solution over the full domain
     n_pts = 500
     x_exact = torch.linspace(0, 1, n_pts).view(-1, 1).requires_grad_(True)
-    y_exact = oscillator.exact_solution(x_exact).view(-1, 1)
+    y_exact = cauchy_problem.reference_solution(x_exact).view(-1, 1)
 
     # sample locations over the problem domain
-    n_col_pts = 100
+    n_col_pts = 30
     collocation_pts = torch.linspace(0, 1,
                                      n_col_pts,
                                      requires_grad=True).view(-1, 1)
@@ -194,88 +232,83 @@ def train_pinns(oscillator: Oscillator):
                                   hidden_dim=32,
                                   nb_layers=3)
 
-    learning_rate = 1e-2
+    coef_loss_residual = 1e-5
+    coef_loss_y0 = 1e1
+    coef_loss_dy0 = 1e-4
+
+    learning_rate = 1e-4
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=learning_rate)
 
-    n_epoch = 50_000
+    n_epoch = 40_000
 
     def lr_multiplier(epoch):
-        return 0.1 ** (epoch / n_epoch)
+        return 1 ** (epoch / n_epoch)
 
     scheduler = lr_scheduler.LambdaLR(optimizer,
                                       lr_lambda=lr_multiplier)
     files = []
-    epoch_list = []
-    loss_residual_list = []
-    loss_y0_list = []
-    loss_dy_dx_0_list = []
-    total_loss_list = []
+    losses = SavedLosses()
     for epoch in tqdm.tqdm(range(n_epoch)):
         optimizer.zero_grad()
         y = model(collocation_pts)
         # computes dy/dx
-        dy_dx = torch.autograd.grad(y, collocation_pts,
-                                    torch.ones_like(y),
-                                    create_graph=True)[0]
+        dy = torch.autograd.grad(y, collocation_pts,
+                                 torch.ones_like(y),
+                                 create_graph=True)[0]
 
         # computes d^2y/dx^2
-        dy_dxx = torch.autograd.grad(dy_dx, collocation_pts,
-                                     torch.ones_like(dy_dx),
-                                     create_graph=True)[0]
+        ddy = torch.autograd.grad(dy, collocation_pts,
+                                  torch.ones_like(dy),
+                                  create_graph=True)[0]
 
         # computes the residual of the 1D harmonic oscillator differential
-        residual = oscillator.residual(dy_dxx, dy_dx, y)
+        residual = cauchy_problem.residual(ddy, dy, y)
         loss_residual = torch.mean(residual ** 2)
-        loss_y0 = (y[0] - 1.0) ** 2
-        loss_dy_dx_0 = dy_dx[0] ** 2
+        cauchy_cond = cauchy_problem.cauchy_conditions(dy, y)
+        loss_y0, loss_dy0 = cauchy_cond.y0 ** 2, cauchy_cond.dy0 ** 2
 
         # backpropagate joint loss
-        alpha = 1e-6
-        loss = alpha * loss_residual + loss_y0 + loss_dy_dx_0
+        loss = (coef_loss_residual * loss_residual
+                + coef_loss_y0 * loss_y0
+                + coef_loss_dy0 * loss_dy0)
 
-        # terms together
         loss.backward()
         optimizer.step()
         scheduler.step()
 
+        losses.save(total=loss.item(),
+                    residual=loss_residual.item(),
+                    init_value=loss_y0.item(),
+                    init_derivative=loss_dy0.item())
+
         # plot the result as training progresses
         if (epoch + 1) % 300 == 0:
             print("")
-            print(f"Learning rate = {optimizer.param_groups[0]['lr']:.2e}")
-            print(f"Losses")
+            print(f"* Learning rate = {optimizer.param_groups[0]['lr']:.2e}")
+            print(f"* Losses")
             print(f"    - Residual loss     = "
                   f"{loss_residual.item():.2e}")
             print(f"    - Initial cond loss = "
-                  f"{loss_y0.item():.2e}   {loss_dy_dx_0.item():.2e}")
+                  f"{loss_y0.item():.2e}   {loss_dy0.item():.2e}")
             print(f"=> Total weighted loss = {loss.item():.2e}")
             yh = model(x_exact).detach()
             xp = collocation_pts.detach()
 
-            file_name = "plots/pinn_%.8i.png" % (epoch + 1)
+            file_name = f"{plots_dir}/pinn_{epoch + 1:8d}.png"
             plot_result(x_exact=x_exact.detach().numpy(),
                         y_exact=y_exact.detach().numpy(),
                         xp=xp, yh=yh, epoch=epoch,
                         file_name=file_name)
             files.append(file_name)
 
-            epoch_list.append(epoch)
-            loss_residual_list.append(loss_residual.item())
-            loss_y0_list.append(loss_y0.item())
-            loss_dy_dx_0_list.append(loss_dy_dx_0.item())
-            total_loss_list.append(loss.item())
-
-    save_gif_PIL("pinn.gif", files, fps=20, loop=0)
-    plot_learning(epoch_list,
-                  loss_residual_list,
-                  loss_y0_list,
-                  loss_dy_dx_0_list,
-                  total_loss_list)
+    save_gif_pil("pinn.gif", files, fps=20, loop=0)
+    plot_losses(losses)
 
 
 if __name__ == '__main__':
     torch.manual_seed(123)
-    oscillator = Oscillator(d=2, w0=20)
+    oscillator = Oscillator(dumping=2, pulsation=20)
     compute_pinn = True
     compute_from_data = False
     if compute_pinn:
